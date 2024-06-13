86a0662d4b37f4494eec7e85158b0a03f254db0c
SeungWoo <wjknn3123@gmail.com>
2024-06-13 14:26:15 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240613/살 수 있는 물건 2/things-able-to-buy-2.cpp
+++ 240613/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -5,6 +5,7 @@ int main() {
     scanf("%d", &n);
     if(n >= 3000) printf("book");
     else if(n >= 1000) printf("mask");
-    else printf("pen");
+    else if(n >= 500) printf("pen");
+    else printf("no");
     return 0;
 }
