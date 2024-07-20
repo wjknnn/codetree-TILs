0bd3cbac59682fd7f816e51d12e396cadc635fd3
SeungWoo <wjknn3123@gmail.com>
2024-07-20 11:15:16 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240720/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const input = String(fs.readFileSync(0))
+
+if(input == 'S') console.log(Superior)
+else if(input == 'A') console.log('Exellent')
+else if(input == 'B') console.log('Good')
+else if(input == 'C') console.log('Usually')
+else if(input == 'D') console.log('Effort')
+else console.log('Failure')
