a9db23b929192a427e23fcf03bfa7212b1889f91
SeungWoo <wjknn3123@gmail.com>
2024-08-28 18:28:25 +0900

    Title: 벌금은 누구에게, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240828/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,16 @@
+n, m, k = map(int, input().split())
+
+arr = [
+    0 for _ in range(n)
+]
+
+for _ in range(n):
+    st = int(input())
+    arr[st - 1] += 1
+
+    for i in range(n):
+        if arr[i] >= k:
+            print(i + 1)
+            return
+
+print(-1)
