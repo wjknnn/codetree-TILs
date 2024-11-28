54838031f4173ae44d8742de704df4f500582d6c
SeungWoo <wjknn3123@gmail.com>
2024-11-28 10:43:39 +0900

    Title: 최소공배수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241128/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 241128/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -1,29 +1,31 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define PB push_back
-#define fi first
-#define se second
-#define all(v) (v).begin(), (v).end()
-#define For(i,j,k) for (int i=(int)(j);i<=(int)(k);i++)
-#define Rep(i,j,k) for (int i=(int)(j);i>=(int)(k);i--)
-#define debug1 puts("--@111%--")
-#define debug2 puts("--@222%--")
-#define debug3 puts("--@333%--")
-#define y1 ysgjowiaef
-using namespace std;
+#include <stdio.h>
 
-#define INF 2123456789
-
-int n, m;
-int g;
+int arr[105];
 
 int main() {
-    cin >> n >> m;
-    For(i, 1, n) {
-        if(n % i == 0 && m % i == 0) g = i;
-    }
-
-    cout << n * m / g;
+	
+	int n, k, m;
+	int trig=0;
+	
+	scanf("%d %d %d",&n,&m,&k);
+	
+	for(int i=1;i<=m;i++)
+	{
+		int v;
+		scanf("%d",&v);
+		arr[v] += 1;
+		if(arr[v]==k)
+		{
+			trig = 1;
+			printf("%d",v);
+			break;
+		}
+	}
+	
+	if(trig==0)
+	{
+		printf("-1");
+	}
+		
+	return 0;
 }
