26ce575967d2fda5037bb2ae47096570ac309bcb
SeungWoo <wjknn3123@gmail.com>
2025-04-29 10:29:25 +0900

    Title: 직사각형 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250429/직사각형 별표 출력하기/print-stars-in-rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
