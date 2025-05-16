35614ef89c8e6601765a5ca98609aeb576e8457e
SeungWoo <wjknn3123@gmail.com>
2025-05-16 13:46:09 +0900

    Title: 별표 출력하기 6, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250516/별표 출력하기 6/print-star-6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N, cnt = 0;
+    scanf("%d", &N);
+    for(int i = 0; i < N * 2 - 1; i++) {
+        for(int j = 0; j < cnt; j++) {
+            printf("  ");
+        }
+        for(int j = 0; j < (N - cnt) * 2 - 1; j++) {
+            printf("* ");
+        }
+        printf("\n");
+        if(i < N - 1) cnt++;
+        else cnt--;
+    }
+    return 0;
+}
